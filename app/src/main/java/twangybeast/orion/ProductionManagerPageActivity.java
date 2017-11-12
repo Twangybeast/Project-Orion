6eb1baf255212e3ee94855e89a19dee242a6562b
TwoTau <TwoTau@users.noreply.github.com>
2017-11-12 04:41:40 -0800

    Fix production improvement spinner

--- app/src/main/java/twangybeast/orion/ProductionManagerPageActivity.java
+++ app/src/main/java/twangybeast/orion/ProductionManagerPageActivity.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class ProductionManagerPageActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ProductionManagerPageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +36,18 @@ public class ProductionManagerPageActivity extends AppCompatActivity {
         cPro.setText("Current Production: " + planet.getCurrentProduction());
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
+        List<Improvement> availableImprovements = FullscreenActivity.gm.pma.getAvailableImprovements(planet);
+        availableImprovements.add(new Improvement(TroopManagerActivity.TROOP_NAME, 0,0,0,TroopManagerActivity.getTroopCost(planet.getOwner())));
+        System.out.println("IMPROVS: " + availableImprovements.size());
+        ArrayList<String> improvements = new ArrayList<String>();
+        for(Improvement improv : availableImprovements) {
+            improvements.add(improv.getName() + "(" + improv.getCost() + ")");
+        }
+        System.out.println(improvements);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item, improvements);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
 
         Button produce = (Button) findViewById(R.id.button);
         produce.setOnClickListener(new View.OnClickListener(){
