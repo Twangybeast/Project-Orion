ace8534f7b457aa12eaa5a49dde6c40fe19e010c
rasberrypai <rasberrypai@gmail.com>
2017-11-11 18:16:44 -0800

    Add drawable planet objects

--- app/src/main/java/twangybeast/orion/DrawingView.java
+++ app/src/main/java/twangybeast/orion/DrawingView.java
@@ -11,32 +11,29 @@ public class DrawingView extends View
     private static final int BACKGROUND = 0xFF080808;
     private Paint paint;
     private Context context;
-    private Set<Planet> planets;
+    private Planet[] planets;
     private int width;
     private int height;
+    private int planetNum = 14;
     public DrawingView(Context context, int width, int height)
     {
         super(context);
         this.context = context;
         this.width = width;
         this.height = height;
-        planets = new HashSet<>();
-        //temp
-        for (int r = 0; r < Config.GRID_HEIGHT; r++)
-        {
-            for (int c = 0; c < Config.GRID_WIDTH; c++)
-            {
-                planets.add(new Planet(c, r, new Random().nextInt(0xFFFFFF)+0xFF000000));
-            }
-        }
         paint = new Paint();
         paint.setColor(0xFF000000);
         paint.setAntiAlias(true);
+        Player natives = new Player(Color.rgb(200, 200, 200));
+        planets = new Planet[] {new Planet(1, 5, natives, 10, 0), new Planet(4, 8, natives, 10, 0), new Planet(8, 16, natives, 10, 0), new Planet(10, 19, natives, 10, 0), new Planet(5, 13, natives, 10, 0), new Planet(2, 1, natives, 10, 0), new Planet(6, 7, natives, 10, 0), new Planet(8, 2, natives, 10, 0)};
+
+
     }
     @Override
     public void onDraw(Canvas canvas)
     {
         canvas.drawColor(BACKGROUND);
+
         for (Planet planet : planets)
         {
             drawPlanet(canvas, paint, planet, 0, 0);
@@ -44,20 +41,8 @@ public class DrawingView extends View
     }
     public void drawPlanet(Canvas canvas, Paint paint, Planet planet, int dx, int dy)
     {
-        paint.setColor(planet.color);
-        Point p = Config.getScreenCoordinates(planet.x, planet.y, width, height);
+        paint.setColor(planet.getColor());
+        Point p = Config.getScreenCoordinates(planet.getPosition().x, planet.getPosition().y, width, height);
         canvas.drawCircle(p.x+Config.getCellWidth(width)/2, p.y+Config.getCellHeight(height)/2, (int)(Config.getPlanetDiameter(width, height)/2), paint);
-    }
-}
-class Planet
-{
-    int color;
-    int x;
-    int y;
-    public Planet(int x, int y, int color)
-    {
-        this.color = color;
-        this.x = x;
-        this.y = y;
     }
 }
--- app/src/main/java/twangybeast/orion/FullscreenActivity.java
+++ app/src/main/java/twangybeast/orion/FullscreenActivity.java
@@ -1,13 +1,6 @@
 package twangybeast.orion;
 
-<<<<<<< HEAD
-import android.annotation.SuppressLint;
-import android.graphics.Color;
-import android.graphics.Point;
-import android.support.v7.app.ActionBar;
-=======
 import android.graphics.Rect;
->>>>>>> refs/remotes/origin/master
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
@@ -17,8 +10,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-
-import java.util.ArrayList;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -33,33 +24,10 @@ public class FullscreenActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_fullscreen);
 
-<<<<<<< HEAD
-        mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
-
-
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-
-
-
-=======
         //idk what i'm doing
         drawingView = new DrawingView(this, getScreenWidth(), getScreenHeight());
         drawingView.setLayoutParams(new ViewGroup.LayoutParams(getScreenWidth(), getScreenHeight()));
         setContentView(drawingView);
->>>>>>> refs/remotes/origin/master
     }
 
     @Override
@@ -98,5 +66,4 @@ public class FullscreenActivity extends AppCompatActivity {
         System.out.println(result);
         return result;
     }
-
 }
